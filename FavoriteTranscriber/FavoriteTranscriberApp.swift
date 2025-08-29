3cf8c24421e7b43470f3157957177bd815330630
Emma Brown <emma.b@comcast.net>
2025-08-29 18:34:35 -0400

    Initial Commit

---
+++ FavoriteTranscriber/FavoriteTranscriberApp.swift
@@ -0,0 +1,17 @@
+//
+//  FavoriteTranscriberApp.swift
+//  FavoriteTranscriber
+//
+//  Created by Emma Brown on 8/29/25.
+//
+
+import SwiftUI
+
+@main
+struct FavoriteTranscriberApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ FavoriteTranscriberTests/FavoriteTranscriberTests.swift
@@ -0,0 +1,17 @@
+//
+//  FavoriteTranscriberTests.swift
+//  FavoriteTranscriberTests
+//
+//  Created by Emma Brown on 8/29/25.
+//
+
+import Testing
+@testable import FavoriteTranscriber
+
+struct FavoriteTranscriberTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
